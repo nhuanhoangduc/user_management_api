2cfbbc42e4a74e72e0fc137800c720c611b9bb91
Nhuan <nhuan.hoangduc@outlook.com>
2018-03-14 22:39:17 +0700

    Add middleware 'cors' and enable CORS for api

--- app.js
+++ app.js
@@ -4,9 +4,14 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var cors = require('cors');
 
 
 var app = express();
+
+
+// Enable CORS
+app.use(cors());
 
 
 // uncomment after placing your favicon in /public
